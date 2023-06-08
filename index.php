f7b9682b741ff74ad5d91b0a43b73fdc39a686d6
Jordan Feigner <jordan.feigner.r@gmail.com>
2023-06-07 18:07:49 -0700

    Boilerplate

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&family=Roboto:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+    <label for="username-input">BGG Username: </label>
+    <input id="username-inpit" type="text">
+    <label for="geeklistID-input">GeekList ID: </label>
+    <input id="geeklistID-input" type="text">
+    <button id="submit">Submit</button>
+    <script src="js/script.js"></script>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.onload = main;
+
+function main() {
+    var submitButton = document.querySelector("#submit");
+    submitButton.addEventListener("click", getMatches);
+}
+
+function getMatches() {
+    
+}
